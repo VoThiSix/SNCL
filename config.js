329c0f86a759601204389074e1a4f54762da7246
unknown <tungtengsl159@gmail.com>
2023-05-23 15:10:48 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,47 @@
+
+var config = {
+    texts: [
+        "Thân gửi Chị Linh❤❤❤",      
+        "Chị Ninh Khỉ nè :)))",  
+        "Hôm nay là sinh nhật chị đúng ko nhỉ ❤❤❤",
+        "Em chúc chị tuổi mới thật nhiều may mắm",
+        "Mãi luôn xinh đẹp,Trẻ mãi không già :D",
+        "Mãi luôn nở 1 nụ cười tươi không cần tưới :Đ",
+        "Kiếm thật là nhiều xiền để hoàn thành mục tiêu 50 củ khoai $$",
+        "Sống lâu Trăm Tuổi :DDD",
+        "Và ",
+        "Mong tình cảm chị em chúng ta sẽ mãi bền lâu ❤❤❤",
+        "Cùng nhau tạo lên những kỉ niệm đáng nhớ ❤❤❤",
+		"Bớt chửi em đi nha chị ning khỉ :DDD",
+        "Chúc Mừng Sinh Nhật Chị eo nha ❤❤❤",
+        "Mãi Keo~~❤❤❤",
+    ],
+    
+    imgs: {
+		"Thân gửi Chị Linh❤❤❤": "./imgs/linh.jpg",
+        "Chị Ninh Khỉ nè :)))": "./imgs/ning.jpg",
+        "Hôm nay là sinh nhật chị đúng ko nhỉ ❤❤❤": "./imgs/bd1.jpg",
+        "Em chúc chị tuổi mới thật nhiều may mắm": "./imgs/mm.jpg",
+        "Mãi luôn xinh đẹp,Trẻ mãi không già :D": "./imgs/pt.jpg",
+        "Mãi luôn nở 1 nụ cười tươi không cần tưới :Đ": "./imgs/ha2.jpg",
+        "Kiếm thật là nhiều xiền để hoàn thành mục tiêu 50 củ khoai $$": "./imgs/tien.jpg",
+        "Sống lâu Trăm Tuổi :DDD": "./imgs/ha1.jpg",
+        "Mong tình cảm chị em chúng ta sẽ mãi bền lâu ❤❤❤": "./imgs/kn1.jpg",
+        "Cùng nhau tạo lên những kỉ niệm đáng nhớ ❤❤❤": "./imgs/kn2.jpg",
+        "Bớt chửi em đi nha chị ning khỉ :DDD": "./imgs/ech.jpg",
+        "Chúc Mừng Sinh Nhật Chị eo nha ❤❤❤": "./imgs/bd2.png",
+		"Mãi Keo~~❤❤❤": "./imgs/haha.jpg",
+        
+},
+    
+    desc: {
+        turn_on: "Bật Đèn",
+        play: "Bật nhạc",
+        bannar_coming: "Nhấn tiếp đi",
+        balloons_flying: "Nhấn tiếp đi",
+        cake_fadein: "Bánh xinh nhật nè",
+        light_candle: "Đốt nến pùm pùm",
+        wish_message: "Ước đi chị",
+        story: "Có vài lời chúc em muốn gửi đến chị",
+    }
+};
---
+++ readme.md
@@ -0,0 +1,49 @@
+## 
+
+> <b>config.jsfork or star~</b>
+
+## TODO
+* [x] 
+* [-] 
+
+### config.js 
+> ！
+```text
+var config = {
+    
+    texts: [
+        "Thân gửi Chị Ning",
+        "Chị Ning Khỉ nè",		
+        "Hôm nay là sinh nhật chị đúng ko nhỉ",  
+        "chúc chị tuổi mới thật nhiều may mắm",
+		"mãi luôn xinh đẹp"
+        "mãi luôn tươi cười",
+		"Sớm kiếm được anh người yêu biết yêu thương chị ",
+        "kiếm thật là nhiều xiền để hoàn thành mục tiêu 50 củ ",
+        "Sống lâu Trăm Tuổi",
+        "Và ",
+        "Mong tình chị em chúng ta sẽ mãi bền lâu <3<3<3",
+		"Bớt chửi em đi nha chị ning khỉ :DDD",
+        "Mãi Keo~~",
+    ],
+    
+    imgs: {
+       "Thân gửi Chị Ning❤❤❤": "./imgs/linh.jpg",
+        "Chị Ning Khỉ nè :)))": "./imgs/ning.jpg",
+        "Hôm nay là sinh nhật chị đúng ko nhỉ": "./imgs/bd1.jpg",
+		"Bớt chửi em đi nha chị ning khỉ :DDD": "./imgs/ech.jpg",
+    },
+    
+    desc: {
+        turn_on: "Nhấn vào đây",
+        play: "Bật nhạc",
+        bannar_coming: "Tiếp theo",
+        balloons_flying: "Tiếp",
+        cake_fadein: "Bánh xinh nhật nè",
+        light_candle: "Đốt nến pùm pùm",
+        wish_message: "Ước đi chị",
+        story: "Có vài lời em muốn lói với chị",
+    }
+};
+```
+
